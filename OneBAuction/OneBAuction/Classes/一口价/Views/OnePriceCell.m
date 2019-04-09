fde3fd88656b530a8da92d5e03c3e5aeb1d25a2c
liushuai <258708917@qq.com>
2019-04-09 09:24:59 +0800

    111

--- OneBAuction/OneBAuction/Classes/一口价/Views/OnePriceCell.m
+++ OneBAuction/OneBAuction/Classes/一口价/Views/OnePriceCell.m
@@ -7,12 +7,23 @@
 //
 
 #import "OnePriceCell.h"
+@interface OnePriceCell()
+@property (weak, nonatomic) IBOutlet UIView *backView;
 
+@end
 @implementation OnePriceCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
+    self.backgroundColor = [UIColor clearColor];
+    UIBezierPath *fieldPath = [UIBezierPath bezierPathWithRoundedRect:_backView.bounds byRoundingCorners:UIRectCornerTopRight | UIRectCornerBottomLeft cornerRadii:CGSizeMake(10 ,10)];
+    CAShapeLayer *fieldLayer = [[CAShapeLayer alloc] init];
+    fieldLayer.frame = _backView.bounds;
+    fieldLayer.path = fieldPath.CGPath;
+    _backView.layer.mask = fieldLayer;
+    _backView.clipsToBounds = YES;
+    
 }
 
 @end
